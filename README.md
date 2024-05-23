cbcec11420dcaabc1358784adf92edb28045b310
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-05-23 11:51:30 -0500

    added the local storage for cards

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # galeweathersdashboard
+
+## Links
+
+<a href= 'https://github.com/galessalazar/galeweathersdashboard'>Gales Weather Dashboard</a><br>
+<a href=''>Update with Deployed Link</a>
+
+## Description
+
+5 day weather forecast and history of searched cities.
+
+## Visuals
+
+<img src='' alt='website screenshot'/>
+<img src='' alt='github screenshot'/>
+
+## Contributors
+
+<a href='https://github.com/galessalazar'>Click here</a>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,86 @@
-const weatherAPI = '0bf01512d291cbf28790b9de360db79a'
-// i copied the url directly from the current weather page, not sure if i should change to the : http://api.openweathermap.org/data/2.5/weather
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather'
-
-//  you'll want to create variables that can hold this input after the user has submitted it.
-
-let cityName = 'san-antonio';
-
-const qURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPI}`
-
-
-
-fetch(qURL)
-
 const searchFormEl = document.querySelector('#search-form');
+const weatherForecastDiv = document.getElementById('weather-cards');
+const weatherAPI = '0bf01512d291cbf28790b9de360db79a'; 
 
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
+searchFormEl.addEventListener('submit', function (event) {  
+    event.preventDefault(); 
+     
+    document.querySelector('#button').addEventListener('click',function(){
+       const cityName = document.getElementById('cityInput').value; 
+    getWeather(cityName)
+    saveSearchedCity(cityName);
+    })
+});
 
-    const searchInputVal = document.querySelector('#search-input')
+function getWeather(cityName) { 
+    const apiURL = 'https://api.openweathermap.org/data/2.5/forecast';
+    const qURL = `${apiURL}?q=${cityName}&appid=${weatherAPI}&units=imperial`; 
 
-    if(!searchInputVal) {
-        console.error('needs input!');
-        return;
+    fetch(qURL)
+        .then(response => {
+            console.log(response);
+            if (!response.ok) {
+               
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayWeatherForecast(data);
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('gale this is broke:', error);
+            
+        });
+}
+
+function displayWeatherForecast(data) {
+    weatherForecastDiv.innerHTML = '';
+
+    for (let i = 0; i < data.list.length; i += 8) { 
+        const forecastItem = data.list[i];
+        const date = new Date(forecastItem.dt * 1000); 
+        const temperature = forecastItem.main.temp;
+        const weatherDescription = forecastItem.weather[0].description;
+
+        const forecastItemElement = document.createElement('div');
+        forecastItemElement.className = 'weather-cards';
+        forecastItemElement.innerHTML = `
+            <h5>Date: ${date.toLocaleDateString()}</h5>
+            <p>Temperature: ${temperature}°F</p>
+            <p>Weather: ${weatherDescription}</p>
+        `;
+        weatherForecastDiv.appendChild(forecastItemElement);
     }
 }
 
-searchFormEl.addEventListener('submit',handleSearchFormSubmit);
+function saveSearchedCity(cityName) {
+    
+    let cities = JSON.parse(localStorage.getItem('previous-cities-list')) || [];
+    cities.push(cityName);
+    localStorage.setItem('previous-cities-list', JSON.stringify(cities));
+    displayPreviousCities();
+}
+
+function displayPreviousCities() {
+    const cities = JSON.parse(localStorage.getItem('previous-cities-list')) || [];
+    
+    const previousCitiesList = document.getElementById('previous-cities-list');
+    previousCitiesList.innerHTML = ''; 
+    const removeDuplicateCities = [...new Set(cities)];
+
+    removeDuplicateCities.forEach(cityName => {
+        const listItem = document.createElement('li');
+        listItem.textContent = cityName;
+        previousCitiesList.appendChild(listItem);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', displayPreviousCities);
+
+// const cityName = document.getElementById('cityInput').value;
+// saveSearchedCity(cityName);
+
+
